9e0d91b9f4bd7acdea87451c907f0ca365eb11fd
Daniel Dias <danieldias.b99@hotmail.com>
2024-05-30 04:05:07 -0300

    feat: part 2 of ticket system implementation

--- src/modules/OnInteraction.js
+++ src/modules/OnInteraction.js
@@ -1,17 +1,105 @@
+/* eslint-disable no-unused-vars */
+const { EmbedBuilder, ButtonBuilder, ActionRowBuilder } = require('discord.js');
+const { Settings } = require('../database/models');
+const DBModule = require('./DBModule');
+
+/**
+ *
+ * @param {import('discord.js').Client} client
+ * @param {import('discord.js').Interaction} interaction
+ * @param {import('./DBModule').Interaction} dbModule
+ * @param {Array<{ data: SlashCommandBuilder, execute: Function }>} slashCommands
+ */
 module.exports = (client, slashCommands) => {
-client.on('interactionCreate', async (interaction) => {
+  client.on('interactionCreate', async (interaction) => {
     try {
       const { message } = interaction;
 
       if (interaction.isButton()) {
-        if(message.content.includes('Lista de Áudios: ')) {
+        if (message.content.includes('Lista de Áudios: ')) {
           const soundpad = slashCommands.get('soundpad');
-          if(!soundpad) return interaction.reply('ERRO: Ocorreu um erro com o SoundPad!');
-          await soundpad.execute(client, interaction);
+          if (!soundpad)
+            return interaction.reply('ERRO: Ocorreu um erro com o SoundPad!');
+          return await soundpad.execute(client, interaction);
+        } else {
+          return;
         }
       }
 
-      if (interaction.isModalSubmit()) return;
+      if (interaction.isModalSubmit()) {
+        if (interaction.customId == 'ticketmodal') {
+          const openTitle = interaction.fields.getTextInputValue('opentitle');
+          const openDescription =
+            interaction.fields.getTextInputValue('opendescription');
+          const ticketTitle =
+            interaction.fields.getTextInputValue('tickettitle');
+          const ticketDescription =
+            interaction.fields.getTextInputValue('ticketdescription');
+          console.log(
+            openTitle,
+            openDescription,
+            ticketTitle,
+            ticketDescription
+          );
+
+          const [settings, created] = await Settings.findOrCreate({
+            where: { id: interaction.guild.id },
+            defaults: {
+              id: interaction.guild.id,
+              ticket_title: ticketTitle,
+              ticket_description: ticketDescription,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            },
+          });
+
+          if (!created) {
+            await settings.update({
+              ticket_title: ticketTitle,
+              ticket_description: ticketDescription,
+              updatedAt: new Date(),
+            });
+          }
+
+          const embedTicket = new EmbedBuilder()
+            .setColor('#2f3136')
+            .setAuthor({
+              name: `${openTitle}`,
+              iconURL: interaction.guild.iconURL({ dynamic: true }),
+            })
+            .setDescription(openDescription)
+            .setFooter({
+              text: interaction.guild.name,
+              iconURL: interaction.guild.iconURL({ dynamic: true }),
+            });
+
+          const dbModule = new DBModule();
+          const { ticket_button_name, ticket_channel_id } =
+            await dbModule.getTicketChannelData(interaction.guildId);
+
+          const botaoTicket = new ButtonBuilder()
+            .setCustomId('abrirticket')
+            .setLabel(ticket_button_name || 'Abrir Ticket')
+            .setStyle(2)
+            .setEmoji('🎯');
+
+          const botoesTicket = new ActionRowBuilder().setComponents(
+            botaoTicket
+          );
+
+          const channel =
+            interaction.guild.channels.cache.get(ticket_channel_id);
+
+          channel.send({ embeds: [embedTicket], components: [botoesTicket] });
+
+          await interaction.reply({
+            content: 'Configurações de ticket criadas com sucesso!',
+            ephemeral: true,
+          });
+        } else {
+          return;
+        }
+      }
 
       if (interaction.isChatInputCommand()) {
         const command = slashCommands.get(interaction.commandName);
@@ -24,7 +112,6 @@ client.on('interactionCreate', async (interaction) => {
 
         return command.execute(client, interaction, slashCommands);
       }
-
     } catch (err) {
       console.error(err);
     }
--- src/modules/TicketModule.js
+++ src/modules/TicketModule.js
@@ -1,23 +1,31 @@
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  EmbedBuilder,
+  PermissionFlagsBits,
+  ChannelType,
+  ButtonStyle,
+} = require('discord.js');
 const { Settings } = require('../database/models');
 const ticketModal = require('../modules/modals/ticketmodal.js');
+const DBModule = require('./DBModule.js');
 
 module.exports = class TicketModule {
   /**
    *
    * @param {import('discord.js').Client} client
-   * @param {import('./DBModule.js')} client.database
+   * @param {import('./DBModule.js')} dbModule
    * @param {import('discord.js').ChatInputCommandInteraction} interaction
    */
+  dbModule = new DBModule();
 
-  async config (client, interaction) {
+  async config(_client, interaction) {
     try {
       const canal = interaction.options.getChannel('canal');
       const canalLogs = interaction.options.getChannel('logs');
       const categoria = interaction.options.getChannel('categoria');
       const botaoTicket = interaction.options.getString('botao');
       const cargo = interaction.options.getRole('cargo');
-
-      console.log(canal, canalLogs, categoria, botaoTicket, cargo);
 
       await Settings.upsert({
         id: interaction.guild.id,
@@ -31,39 +39,203 @@ module.exports = class TicketModule {
         ticket_role_id: cargo.id,
         ticket_title: botaoTicket,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       });
 
-      await interaction.showModal(ticketModal);
-
-    return;
+      return await interaction.showModal(ticketModal);
     } catch (error) {
       console.error(error);
     }
   }
 
-  
   async addUser(client, interaction) {
     this.checkTicketConfig(client, interaction);
   }
-  
+
   async removeUser(client, interaction) {
-    console.log(client, interaction);    
+    this.checkTicketConfig(client, interaction);
   }
-  
+
   async checkTicketConfig(client, interaction) {
-    return interaction.reply({ });
-
-    // if (
-    //   !guildSettings ||
-    //   !guildSettings.ticket_category_id ||
-    //   !guildSettings.ticket_role_id
-    // ) {
-    //   return interaction.reply({
-    //     content: '❌ **O sistema de tickets não foi configurado!**',
-    //     ephemeral: true,
-    //   });
-    // }
-
+    const hasAdminRole = interaction.memberPermissions?.has([
+      PermissionFlagsBits.Administrator,
+    ]);
+    if (!hasAdminRole) return interaction.reply('Erro: Não Autorizado!!!');
+    return;
   }
-}
+
+  async ticketModal(client, interaction) {
+    const openTitle = interaction.fields.getTextInputValue('opentitle');
+    const openDescription =
+      interaction.fields.getTextInputValue('opendescription');
+    const ticketTitle = interaction.fields.getTextInputValue('tickettitle');
+    const ticketDescription =
+      interaction.fields.getTextInputValue('ticketdescription');
+
+    const [settings, created] = await Settings.findOrCreate({
+      where: { id: interaction.guild.id },
+      defaults: {
+        id: interaction.guild.id,
+        ticket_title: ticketTitle,
+        ticket_description: ticketDescription,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
+
+    if (!created) {
+      await settings.update({
+        ticket_title: ticketTitle,
+        ticket_description: ticketDescription,
+        updatedAt: new Date(),
+      });
+    }
+
+    const embedTicket = new EmbedBuilder()
+      .setColor('#2f3136')
+      .setAuthor({
+        name: `${openTitle}`,
+        iconURL: interaction.guild.iconURL({ dynamic: true }),
+      })
+      .setDescription(openDescription)
+      .setFooter({
+        text: interaction.guild.name,
+        iconURL: interaction.guild.iconURL({ dynamic: true }),
+      });
+
+    const { ticket_button_name, ticket_channel_id } =
+      await this.dbModule.getGuildData(interaction.guildId);
+
+    const botaoTicket = new ButtonBuilder()
+      .setCustomId('ticket-open')
+      .setLabel(ticket_button_name || 'Abrir Ticket')
+      .setStyle(2)
+      .setEmoji('🎯');
+
+    const botoesTicket = new ActionRowBuilder().setComponents(botaoTicket);
+
+    const channel = interaction.guild.channels.cache.get(ticket_channel_id);
+
+    channel.send({ embeds: [embedTicket], components: [botoesTicket] });
+
+    await interaction.reply({
+      content: 'Configuração do sistema de ticket realizada com sucesso!',
+      ephemeral: true,
+    });
+  }
+
+  async ticketOpen(client, interaction) {
+    if (
+      interaction.guild.channels.cache.find(
+        (c) => c.topic === interaction.user.id
+      )
+    ) {
+      return interaction.reply({
+        content: `**Já existe um ticket aberto para a sua conta -> ${interaction.guild.channels.cache.find(
+          (c) => c.topic === interaction.user.id
+        )}.**`,
+        ephemeral: true,
+      });
+    }
+
+    const guildData = await this.dbModule.getGuildData(interaction.guildId);
+
+    const { ticket_category_id, ticket_role_id } = guildData;
+
+    const ticketChannel = await interaction.guild.channels.create({
+      name: `🔖・ticket--${interaction.user.username}`,
+      type: ChannelType.GuildText,
+      topic: `${interaction.user.id}`,
+      parent: ticket_category_id,
+      permissionOverwrites: [
+        {
+          id: ticket_role_id,
+          allow: [
+            PermissionFlagsBits.ViewChannel,
+            PermissionFlagsBits.SendMessages,
+            PermissionFlagsBits.AttachFiles,
+            PermissionFlagsBits.EmbedLinks,
+            PermissionFlagsBits.AddReactions,
+          ],
+        },
+        {
+          id: interaction.guild.id,
+          deny: [PermissionFlagsBits.ViewChannel],
+        },
+        {
+          id: interaction.user.id,
+          allow: [
+            PermissionFlagsBits.ViewChannel,
+            PermissionFlagsBits.SendMessages,
+            PermissionFlagsBits.AttachFiles,
+            PermissionFlagsBits.EmbedLinks,
+            PermissionFlagsBits.AddReactions,
+          ],
+        },
+      ],
+    });
+
+    const { embedCreated, buttonRowCreated } = await this.embedCreation(
+      client,
+      interaction
+    );
+
+    await interaction.reply({
+      embeds: [embedCreated],
+      components: [buttonRowCreated],
+      ephemeral: true,
+    });
+
+    await this.embedTicket(client, interaction, guildData, ticketChannel);
+  }
+
+  async embedCreation(_client, interaction) {
+    const embedCreated = new EmbedBuilder()
+      .setColor('#2f3136')
+      .setAuthor({
+        name: `Suporte - ${interaction.guild.name}`,
+        iconURL: interaction.guild.iconURL({ dynamic: true }),
+      })
+      .setDescription(
+        `Olá ${interaction.user}, Seu ticket foi criado com sucesso.`
+      )
+      .setFooter({
+        text: interaction.guild.name,
+        iconURL: interaction.guild.iconURL({ dynamic: true }),
+      });
+
+    const button = new ButtonBuilder()
+      .setLabel('Atalho')
+      .setURL(interaction.channel.url)
+      .setStyle(ButtonStyle.Link);
+
+    const buttonRowCreated = new ActionRowBuilder().addComponents(button);
+
+    return { embedCreated, buttonRowCreated };
+  }
+
+  async embedTicket(client, interaction, guildData, ticketChannel) {
+    const ticketChannelEmbed = new EmbedBuilder()
+      .setColor('#2f3136')
+      .setAuthor({
+        name: guildData.ticket_title,
+        iconURL: interaction.guild.iconURL({ dynamic: true }),
+      })
+      .setDescription(guildData.ticket_description)
+      .setFooter({
+        text: interaction.guild.name,
+        iconURL: interaction.guild.iconURL({ dynamic: true }),
+      });
+
+    const closeButton = new ButtonBuilder()
+      .setCustomId('ticket-close')
+      .setStyle(ButtonStyle.Danger)
+      .setLabel('Fechar ticket');
+
+    const closeButtonRow = new ActionRowBuilder().addComponents(closeButton);
+
+    ticketChannel.send({ embeds: [ticketChannelEmbed], components: [closeButtonRow] }).then((m) => {
+      m.pin();
+    });
+  }
+};
