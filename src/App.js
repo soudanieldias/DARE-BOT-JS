ca8907dc3043719338d4531c210e13787d7b6ea1
soudanieldias <soudanieldias@hotmail.com>
2025-05-17 22:35:45 -0300

    feat: modify project structure

--- src/App.js
+++ src/App.js
@@ -22,36 +22,78 @@ const {
 
 dotenv.config();
 
+/**
+ * Classe principal da aplicação Discord
+ * @class App
+ */
 class App {
+  /**
+   * @type {Client}
+   */
   client = new Client({
     partials: [...partialsList],
     intents: [...intentsList],
   });
 
+  /**
+   * @type {string}
+   */
   TOKEN = process.env.TOKEN;
 
+  /**
+   * @type {Collection}
+   */
   slashCommands = new Collection();
-  
 
+  /**
+   * Inicializa uma nova instância da aplicação
+   * @throws {Error} Se o TOKEN não estiver configurado
+   */
   constructor() {
-    this.client.adminModule = new AdminModule();
-    this.client.database = new DatabaseModule(this.client);
-    this.client.pads = new Collection();
-    this.client.soundModule = new SoundModule();
-    this.client.soundpadModule = new SoundpadModule();
-    this.client.ticketModule = new TicketModule();
-    this.client.embedModule = new EmbedModule();
-    this.client.memeLoaderModule = new MemeLoaderModule();
+    if (!this.TOKEN) {
+      throw new Error('TOKEN não configurado no arquivo .env');
+    }
+
+    this.initializeModules();
+  }
+
+  /**
+   * Inicializa todos os módulos necessários
+   * @private
+   */
+  initializeModules() {
+    try {
+      this.client.adminModule = new AdminModule();
+      this.client.database = new DatabaseModule(this.client);
+      this.client.pads = new Collection();
+      this.client.soundModule = new SoundModule();
+      this.client.soundpadModule = new SoundpadModule();
+      this.client.ticketModule = new TicketModule();
+      this.client.embedModule = new EmbedModule();
+      this.client.memeLoaderModule = new MemeLoaderModule();
+    } catch (error) {
+      console.error('Erro ao inicializar módulos:', error);
+      throw error;
+    }
   }
   
+  /**
+   * Inicia a aplicação e carrega todos os módulos necessários
+   * @throws {Error} Se houver erro durante a inicialização
+   */
   start() {
-    ActivityModule.default(this.client);
-    InteractionModule(this.client, this.slashCommands);
-    OnReadyModule(this.client, this.TOKEN);
-    OnVoiceModule(this.client);
-    CommandLoaderModule(this.client, this.slashCommands);
-    ButtonLoaderModule(this.client);
-    this.client.soundpadModule.start(this.client);
+    try {
+      ActivityModule.default(this.client);
+      InteractionModule(this.client, this.slashCommands);
+      OnReadyModule(this.client, this.TOKEN);
+      OnVoiceModule(this.client);
+      CommandLoaderModule(this.client, this.slashCommands);
+      ButtonLoaderModule(this.client);
+      this.client.soundpadModule.start(this.client);
+    } catch (error) {
+      console.error('Erro ao iniciar a aplicação:', error);
+      throw error;
+    }
   }
 }
 
